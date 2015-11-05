97933df6115c5036a0d65dd14f0cc4df1961a88c
Robin Chatfield <robin.chatfield@gmail.com>
2015-11-05 09:53:00 +0100

    Begining of new method for generating images.

--- website_imagemagick/image_recipe.py
+++ website_imagemagick/image_recipe.py
@@ -74,7 +74,7 @@ class website_imagemagic(http.Controller):
         if recipe_ref:
             recipe = request.env.ref(recipe_ref) # 'imagemagick.my_recipe'
         #~ recipe.send_file(http,field=field,model=model,id=id.split('_')[0])
-        recipe.send_file(http,field=field,model=model,id=id)
+        return recipe.send_file(http,field=field,model=model,id=id)
 
 
         try:
@@ -85,10 +85,124 @@ class website_imagemagic(http.Controller):
                 request.cr, request.uid, model, id, field, response, max_width, max_height,
                 cache=STATIC_CACHE if len(idsha) > 1 else None)
         except Exception:
-            logger.exception("Cannot render image field %r of record %s[%s] at size(%s,%s)",
+            _logger.exception("Cannot render image field %r of record %s[%s] at size(%s,%s)",
                              field, model, id, max_width, max_height)
             response = werkzeug.wrappers.Response()
             return self.placeholder(response)
+
+class website(models.Model):
+    _inherit = 'webiste'
+    
+    #~ def _image_placeholder(self, response):
+        #~ """
+        #~ Choose placeholder.
+        #~ """
+        #~ # file_open may return a StringIO. StringIO can be closed but are
+        #~ # not context managers in Python 2 though that is fixed in 3
+        #~ with contextlib.closing(openerp.tools.misc.file_open(
+                #~ os.path.join('web', 'static', 'src', 'img', 'placeholder.png'),
+                #~ mode='rb')) as f:
+            #~ response.data = f.read()
+            #~ return response.make_conditional(request.httprequest)
+    
+    def imagemagick_url(self, record, field, recipe):
+        """Returns a local url that points to the image field of a given browse record."""
+        model = record._name
+        sudo_record = record.sudo()
+        sudo_recipe = recipe.sudo()
+        id = '%s_%s' % (record.id, hashlib.sha1('%s%s' % (sudo_record.write_date or sudo_record.create_date or '',
+            sudo_recipe.write_date or sudo_recipe.create_date or '')).hexdigest()[0:7])
+        return '/website/image/%s/%s/%s/%s' % (model, id, field, recipe.id)
+    
+    # WIP. Very temporary solution.
+    def imagemagick(self, model, id, field, recipe, response, cache=None):
+        """ Fetches the requested field and applies the given imagemagick recipe on it.
+
+        ???Resizing is bypassed if the object provides a $field_big, which will
+        be interpreted as a pre-resized version of the base field.???
+
+        If the record is not found or does not have the requested field,
+        returns a placeholder image via :meth:`~._image_placeholder`.
+
+        Sets and checks conditional response parameters:
+        * :mailheader:`ETag` is always set (and checked)
+        * :mailheader:`Last-Modified is set iif the record has a concurrency
+          field (``__last_update``)
+
+        The requested field is assumed to be base64-encoded image data in
+        all cases.
+        """
+        
+        return recipe.send_file(http,field=field,model=model,id=id)
+        
+        Model = self.pool[model]
+        id = int(id)
+
+        ids = Model.search(cr, uid,
+                           [('id', '=', id)], context=context)
+        if not ids and 'website_published' in Model._fields:
+            ids = Model.search(cr, openerp.SUPERUSER_ID,
+                               [('id', '=', id), ('website_published', '=', True)], context=context)
+        if not ids:
+            return self._image_placeholder(response)
+
+        concurrency = '__last_update'
+        [record] = Model.read(cr, openerp.SUPERUSER_ID, [id],
+                              [concurrency, field],
+                              context=context)
+
+        if concurrency in record:
+            server_format = openerp.tools.misc.DEFAULT_SERVER_DATETIME_FORMAT
+            try:
+                response.last_modified = datetime.datetime.strptime(
+                    record[concurrency], server_format + '.%f')
+            except ValueError:
+                # just in case we have a timestamp without microseconds
+                response.last_modified = datetime.datetime.strptime(
+                    record[concurrency], server_format)
+
+        # Field does not exist on model or field set to False
+        if not record.get(field):
+            # FIXME: maybe a field which does not exist should be a 404?
+            return self._image_placeholder(response)
+
+        response.set_etag(hashlib.sha1(record[field]).hexdigest())
+        response.make_conditional(request.httprequest)
+
+        if cache:
+            response.cache_control.max_age = cache
+            response.expires = int(time.time() + cache)
+
+        # conditional request match
+        if response.status_code == 304:
+            return response
+
+        data = record[field].decode('base64')
+        image = Image.open(cStringIO.StringIO(data))
+        response.mimetype = Image.MIME[image.format]
+
+        filename = '%s_%s.%s' % (model.replace('.', '_'), id, str(image.format).lower())
+        response.headers['Content-Disposition'] = 'inline; filename="%s"' % filename
+
+        if (not max_width) and (not max_height):
+            response.data = data
+            return response
+
+        w, h = image.size
+        max_w = int(max_width) if max_width else maxint
+        max_h = int(max_height) if max_height else maxint
+
+        if w < max_w and h < max_h:
+            response.data = data
+        else:
+            size = (max_w, max_h)
+            img = image_resize_and_sharpen(image, size, preserve_aspect_ratio=True)
+            image_save_for_web(img, response.stream, format=image.format)
+            # invalidate content-length computed by make_conditional as
+            # writing to response.stream does not do it (as of werkzeug 0.9.3)
+            del response.headers['Content-Length']
+
+        return response
 
 class image_recipe(models.Model):
     _name = "image.recipe"
@@ -105,11 +219,11 @@ class image_recipe(models.Model):
             path = '/'.join(get_module_path(attachment.url.split('/')[1]).split('/')[0:-1])
             return Image(filename=path + attachment.url)
         _logger.warning('<<<<<<<<<<<<<< attachment_to_img >>>>>>>>>>>>>>>>: %s' % attachment.datas)
-        return Image(StringIO(attachment.datas).decode('base64'))
+        return Image(blob=attachment.datas.decode('base64'))
 
     def data_to_img(self, data):  # return an image object while filename is data
         _logger.warning('<<<<<<<<<<<<<< data_to_img >>>>>>>>>>>>>>>>: %s' % data)
-        return Image(StringIO(data).decode('base64'))
+        return Image(blob=data.decode('base64'))
 
     def url_to_img(self, url):  # return an image object while filename is an url
         return Image(filename=url)
@@ -123,7 +237,7 @@ class image_recipe(models.Model):
         if field:
             o = self.env[model].browse(int(id))
             _logger.warning('<<<<<<<<<<<<<< data >>>>>>>>>>>>>>>>: %s' % o)
-            return http.send_file(StringIO(self.run(self.data_to_img(o.read()[0][field])).make_blob(format='jpg')), filename=field, mtime=self.get_mtime(o))
+            return http.send_file(StringIO(self.run(self.data_to_img(getattr(o, field))).make_blob(format='jpg')), filename=field, mtime=self.get_mtime(o))
         if attachment:
             _logger.warning('<<<<<<<<<<<<<< attachment >>>>>>>>>>>>>>>>: %s' % attachment)
             return http.send_file(StringIO(self.run(self.attachment_to_img(attachment)).make_blob(format='jpg')), filename=attachment.datas_fname, mtime=self.get_mtime(attachment))
