1e5ddea6312ce85ca68ce5dda48b60246e7cd964
Haojun Zou <apollo_zhj@msn.com>
2018-01-15 12:28:53 +0100

    website_memcached_imagemagick: new parameter in controller

--- website_memcached_imagemagick/website.py
+++ website_memcached_imagemagick/website.py
@@ -24,47 +24,47 @@ from openerp import http
 from openerp.addons.web.http import request
 from openerp.addons.website_memcached import memcached
 
-from openerp.addons.website.website_imagemagic import website_imagemagic
+from openerp.addons.website_imagemagick.image_recipe import website_imagemagic
 
 import logging
 _logger = logging.getLogger(__name__)
 
-    
+
 class CachedImageMagick(website_imagemagic):
-    
+
     #~ @http.route(['/imagemagick/<model("ir.attachment"):image>/id/<model("image.recipe"):recipe>',
                  #~ '/imagemagick/<model("ir.attachment"):image>/ref/<string:recipe_id>'], type='http', auth="public", website=True)
-    @memcached@route(flush_type='imagemagick',binary=True)
+    @memcached.route(flush_type='imagemagick',binary=True, key=lambda k: '{db},{path},{lang}', max_age=31536000, cache_age=60*60*24*30)
     def view_attachment(self, image=None, recipe=None, recipe_ref=None, **post):
-        return super(CachedWebsite, self).view_attachment(image, recipe, recipe_ref, **post)
-  
+        return super(CachedImageMagick, self).view_attachment(image, recipe, recipe_ref, **post)
+
 
     #~ @http.route(['/imageurl/<string:url>/id/<model("image.recipe"):recipe>','/imageurl/<string:url>/ref/<string:recipe>'], type='http', auth="public", website=True)
-    @memcached@route(flush_type='imagemagick',binary=True,send_file=True)
+    @memcached.route(flush_type='imagemagick',binary=True, key=lambda k: '{db},{path},{lang}', max_age=31536000, cache_age=60*60*24*30)
     def view_url(self, url=None, recipe=None, recipe_ref=None, **post):
-        return super(CachedWebsite, self).view_url(url, recipe, recipe_ref, **post)
+        return super(CachedImageMagick, self).view_url(url, recipe, recipe_ref, **post)
 
 
     #~ @http.route([
         #~ '/imagefield/<model>/<field>/<id>/ref/<recipe_ref>',
         #~ '/imagefield/<model>/<field>/<id>/id/<model("image.recipe"):recipe>',
         #~ ], type='http', auth="public", website=True, multilang=False)
-    @memcached@route(flush_type='imagemagick',binary=True,send_file=True)
+    @memcached.route(flush_type='imagemagick',binary=True, key=lambda k: '{db},{path},{lang}', max_age=31536000, cache_age=60*60*24*30)
     def website_image(self, model, id, field, recipe=None,recipe_ref=None):
-        return super(CachedWebsite, self).website_image(model, id, field, recipe,recipe_ref)
+        return super(CachedImageMagick, self).website_image(model, id, field, recipe,recipe_ref)
 
     #~ @http.route([
         #~ '/imagefieldurl/<model>/<field>/<id>/ref/<recipe_ref>',
         #~ '/imagefieldurl/<model>/<field>/<id>/id/<model("image.recipe"):recipe>',
         #~ ], type='http', auth="public", website=True, multilang=False)
-    @memcached@route(flush_type='imagemagick',binary=True,send_file=True)
+    @memcached.route(flush_type='imagemagick',binary=True, key=lambda k: '{db},{path},{lang}', max_age=31536000, cache_age=60*60*24*30)
     def website_url(self, model, id, field, recipe=None,recipe_ref=None):
-        return super(CachedWebsite, self).website_url(model, id, field, recipe,recipe_ref)
+        return super(CachedImageMagick, self).website_url(model, id, field, recipe,recipe_ref)
 
 
     #~ @http.route([
         #~ '/website/imagemagick/<model>/<field>/<id>/<model("image.recipe"):recipe>',
         #~ ], type='http', auth="public", website=True, multilang=False)
-    @memcached@route(flush_type='imagemagick',binary=True)
+    @memcached.route(flush_type='imagemagick',binary=True, key=lambda k: '{db},{path},{lang}', max_age=31536000, cache_age=60*60*24*30)
     def website_imagemagick(self, model, field, id, recipe=None):
-        return super(CachedWebsite, self).website_imagemagick(model, field, id, recipe)
+        return super(CachedImageMagick, self).website_imagemagick(model, field, id, recipe)
