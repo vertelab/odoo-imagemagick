a14495e083bf5eae39f649b7fd6bec7f87dc8966
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-09-15 11:57:29 +0200

    Update __manifest__.py

--- website_imagemagick/__manifest__.py
+++ website_imagemagick/__manifest__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Odoo, Open Source Enterprise Management Solution, third party addon
-#    Copyright (C) 2014-2016 Vertel AB (<http://vertel.se>).
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -15,15 +15,16 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
 {
-    'name': 'Image Magick',
-    'version': '1.0',
-    'category': 'Website',
-    'licence': 'AGPL-3',
+    'name': 'Image: Imagemagick: Website',
+    'version': '14.0.0.0.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Advanced Image handling',
+    'category': 'Technical',
     'description': """
 Advanced Image handling
 =======================
@@ -36,8 +37,14 @@ Advanced Image handling
 
 
 """,
+    #'sequence': '1',
     'author': 'Vertel AB',
-    'website': 'http://www.vertel.se',
+    'website': 'https://vertel.se/apps/odoo-imagemagick/imagemagick_website',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-imagemagick',
     'depends': ['base', 'website'],
     'external_dependencies': {
         'python': ['wand', ],
@@ -54,3 +61,4 @@ Advanced Image handling
     'application': True,
     # 'installable': True,
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
